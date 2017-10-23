550c994bb9987eb498da4a1c930b54143ea8f9f3
Edward George <edwardgeorge@gmail.com>
2017-10-23 16:17:16 +0100

    implemented a config stack for obtaining runtime args

--- nstack/__init__.py
+++ nstack/__init__.py
@@ -1,6 +1,48 @@
+import collections
 import six
+import threading
 
 modules = {}
+
+class LocalConfig(threading.local):
+    def __init__(self):
+        self.stack = collections.deque([{}])
+
+    def push(self, config):
+        self.stack.appendleft(config)
+
+    def pop(self):
+        self.stack.popleft()
+
+    def peek(self):
+        try:
+            return self.stack[0]
+        except LookupError:
+            raise LookupError("Empty config")
+
+    def __getitem__(self, name):
+        return self.peek()[name]
+
+    def get(self, name, default=None):
+        return self.peek().get(name, default)
+
+config = LocalConfig()
+
+def with_config(conf, f=None):
+    def decorator(f):
+        def decorated(*a, **k):
+            config.push(conf)
+            try:
+                return f(*a, **k)
+            finally:
+                config.pop()
+        return decorated
+    if f is not None:
+        return decorator(f)
+    return decorator
+
+def use_config(config):
+    config.push(config)
 
 class Module_(object):
     def __init__(self, name):
@@ -29,4 +71,5 @@ class Registry(type):
 
 @six.add_metaclass(Registry)
 class Module(object):
-    pass
+    def __init__(self):
+        self.args = config
